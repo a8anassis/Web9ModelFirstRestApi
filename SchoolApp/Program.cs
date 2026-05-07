6433a5f82bd612fb5112ffc0e884f2b87b921c86
a8anassis <a8anassis@gmail.com>
2026-05-07 21:03:08 +0300

    Implement layered architecture with auth, DI, and logging
    Introduced DTOs, repositories, services, and custom exceptions for a school management system. Added BCrypt password encryption, JWT authentication, and AutoMapper integration. Configured dependency injection and logging with Serilog. Removed WeatherForecast sample code and updated project structure.

--- SchoolApp/Program.cs
+++ SchoolApp/Program.cs
@@ -1,6 +1,13 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SchoolApp.Configuration;
 using SchoolApp.Data;
+using SchoolApp.Repositories;
+using SchoolApp.Security;
+using SchoolApp.Services;
+using System.Text;
 
 namespace SchoolApp
 {
@@ -14,6 +21,42 @@ namespace SchoolApp
 
             builder.Services.AddDbContext<SchoolMvc9Context>(options =>
                     options.UseSqlServer(connString));
+
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<ITeacherService, TeacherService>();
+            builder.Services.AddScoped<IStudentService, StudentService>();
+            builder.Services.AddScoped<IApplicationService, ApplicationService>();
+            builder.Services.AddSingleton<IEncryptionUtil, EncryptionUtil>();
+
+            builder.Services.AddRepositories();
+
+            builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MapperConfig>());
+
+            var jwtSettings = builder.Configuration.GetSection("Jwt");
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                //options.IncludeErrorDetails = builder.Environment.IsDevelopment();  // χρήσιμο σε development, δείχνει αναλυτικά errors. Στο production βάζουμε false.
+                // options.SaveToken = true; αποθηκεύει το token στο HttpContext ώστε να μπορούμε να το διαβάσουμε μετά με HttpContext.GetTokenAsync("access_token")
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings["Audience"],
+
+                    ValidateLifetime = true,
+
+                    ValidateIssuerSigningKey = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Secret"]!))
+                };
+            });
+
 
             // Add services to the container.
 
@@ -31,6 +74,7 @@ namespace SchoolApp
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
